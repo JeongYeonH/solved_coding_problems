f21fae16f2f6c2dd0b85b3e162e7f32a6b9c577b
Jeong Yeon Hwang <kukuro1995@naver.com>
2025-06-21 00:45:11 +0900

    3래벨 치곤 수월했음

---
+++ 레벨3_문제/야근_지수.java
@@ -0,0 +1,45 @@
+package 레벨3_문제;
+
+import java.util.Arrays;
+
+public class 야근_지수 {
+     public long solution(int n, int[] works) {      
+        if(works.length == 0){
+            int finalAmount = (works[0] - n) >= 0 ? (works[0]-n) : 0;
+            return (long) Math.pow(finalAmount, 2);
+        }
+        
+        Arrays.sort(works);
+        int [] reversedWorks = new int[works.length];
+        for(int i = 0; i < works.length; i++){
+             reversedWorks[i] = works[works.length-1 - i];
+        }
+        int currIdx = 0;
+        
+        outer:
+        while(n > 0){      
+            int currValue = reversedWorks[currIdx];
+            while(reversedWorks[currIdx] == currValue && n > 0){             
+                if (n == 0) break;    
+                if (reversedWorks[currIdx] == 0) break outer;
+                reversedWorks[currIdx] -=1;
+                n--; 
+                currIdx++;
+                if(currIdx == reversedWorks.length -1){
+                    if( n > 0){
+                        reversedWorks[currIdx] -=1;
+                        n--;
+                        break;
+                    }else if( n == 0) break;
+                }                  
+            }          
+            currIdx = 0;
+        }
+        
+        long answer = 0;
+        for(int work : reversedWorks){
+            answer += Math.pow(work, 2);
+        }
+        return answer;
+    }   
+}
