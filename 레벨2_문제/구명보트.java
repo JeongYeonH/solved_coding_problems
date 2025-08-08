402048d8dac971ccfce91e95e9425a4253a50f5c
Jeong Yeon Hwang <kukuro1995@naver.com>
2025-08-08 17:52:30 +0900

    그냥 수정

--- 레벨2_문제/구명보트.java
+++ 레벨2_문제/구명보트.java
@@ -6,7 +6,7 @@ public class 구명보트 {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-       
+          
         int left = 0;
         int right = people.length - 1;
         while(left <= right){
--- 레벨2_문제/요격_시스템.java
+++ 레벨2_문제/요격_시스템.java
@@ -1,13 +1,13 @@
 package 레벨2_문제;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class 요격_시스템 {
     public int solution(int[][] targets) {
         int answer = 0;
         int[][] sortedTargets = Arrays.stream(targets)
                                     .sorted((a,b) -> a[1]-b[1])
-                                    .toArray(size -> new int[size][]);
+                                    .toArray(size -> new int[size][]);                                    
         double launchPoint = -1;
         for(int i = 0; i < targets.length; i++){
             int[] target = sortedTargets[i];
