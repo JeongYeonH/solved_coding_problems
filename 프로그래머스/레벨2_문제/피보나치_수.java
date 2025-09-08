945dea21726ccc490ee50ba80c105f3163fd3116
Jeong Yeon Hwang <kukuro1995@naver.com>
2025-09-08 22:34:55 +0900

    쉬운 문제

---
+++ 프로그래머스/레벨2_문제/피보나치_수.java
@@ -0,0 +1,14 @@
+package 레벨2_문제;
+
+public class 피보나치_수 {
+    public int solution(int n) {
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2; i <= n; i++){
+            arr[i] = (arr[i - 1]%1234567)+ (arr[i - 2]%1234567);
+        }
+        
+        return (arr[n]%1234567);
+    }
+}
