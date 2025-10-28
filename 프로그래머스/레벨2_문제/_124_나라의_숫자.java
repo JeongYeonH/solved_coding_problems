47efb3aac7f28845d4f9e35c25a0af60212fb6b9
Jeong Yeon Hwang <kukuro1995@naver.com>
2025-10-28 23:27:24 +0900

    이해를 못하겠어..

---
+++ 프로그래머스/레벨2_문제/_124_나라의_숫자.java
@@ -0,0 +1,21 @@
+package 레벨2_문제;
+
+public class _124_나라의_숫자 {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+        
+        while(n > 0){
+            int num = n%3;
+            
+            if(num == 0){
+                answer.append(4);
+                n = n/3 - 1;
+            }else{
+                answer.append(num);
+                n = n/3;
+            }
+        }
+        
+        return answer.reverse().toString();
+    }   
+}
